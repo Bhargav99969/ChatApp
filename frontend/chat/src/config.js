dd5fea50bb423e5c4fb23789e0d5379db327c458
Bhargav99969 <unthale@gmail.com>
2025-06-28 23:51:26 +0530

    url updated

--- frontend/chat/src/config.js
+++ frontend/chat/src/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://chatappbackend-tc7v.onrender.com/api",
     withCredentials:true,
 
 })
