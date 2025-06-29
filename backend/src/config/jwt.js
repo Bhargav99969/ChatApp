9c2fdf50c5ba90a84861e1469538eb4ef648d5ec
Bhargav99969 <unthale@gmail.com>
2025-06-29 14:58:23 +0530

    new

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -8,9 +8,8 @@ export const genrateToken = (userid, res) => {
   });
   res.cookie("token", token, {
     maxAge: 10 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production", 
-    sameSite:"None"
+    sameSite:"None",
+    secure:process.env.NODE_ENV !=="development"
   });
 
   return token
