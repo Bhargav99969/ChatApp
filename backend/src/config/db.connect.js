7358dafdb4cff13b2d1207aa02d29b7d658073f4
Bhargav99969 <unthale@gmail.com>
2025-07-05 17:15:48 +0530

    3

--- backend/src/config/db.connect.js
+++ backend/src/config/db.connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-  const conn=await  mongoose.connect(process.env.MONGO);
+  const conn=await  mongoose.connect("mongodb+srv://bhanu:Bhargav@cluster0.xqvzyr8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("connected mongo");
   } catch (erro) {
     console.log(erro);
